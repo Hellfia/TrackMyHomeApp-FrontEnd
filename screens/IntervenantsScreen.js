94c3623774c3978cddd7760c8b5be46206e01394
Hellfia <boulangerkevin@ymail.com>
2025-03-04 18:09:05 +0100

    mise en place craftsman container + page intervenant

--- screens/IntervenantsScreen.js
+++ screens/IntervenantsScreen.js
@@ -7,19 +7,20 @@ import {
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import PlusButton from "../components/PlusButton";
-
+import { useSelector } from "react-redux";
 import CraftsmanContainer from "../components/CraftsmanContainer";
 import globalStyles from "../styles/globalStyles";
 import { useEffect, useState } from "react";
+
 export default function MesIntervenants({ navigation }) {
   const [craftsman, setCraftsman] = useState([]);
-  const Craftsman = useSelector((state) => state.Craftsman);
 
   useEffect(() => {
-    const craftsmanId = "67c72424a42cfad1eaae00bc";
-    fetch(`http://192.168.1.146:4000/craftsmen/${craftsmanId}`)
+    const constructorId = "67c72424a42cfad1eaae00bc";
+    fetch(`http://192.168.1.146:4000/projects/craftsmen/${constructorId}`)
       .then((res) => res.json())
       .then((data) => {
+        console.log("dataFecthed", data);
         setCraftsman(data.data);
       });
   }, []);
@@ -35,21 +36,21 @@ export default function MesIntervenants({ navigation }) {
           Mes Intervenants
         </Text>
         <ScrollView>
-          {Craftsman.length > 0 ? (
-            Craftsman.filter(
-              (craftsmanItem) => craftsmanItem.Craftsman !== null
-            ).map((craftsmanItem) => (
-              <ClientContainer
-                key={craftsmanItem._id}
-                craftsmanName={craftsmanItem.client.firstname}
-                craftsmanAddress={craftsmanItem.client.constructionAdress}
-                craftsmanZip={craftsmanItem.client.constructionZipCode}
-                craftsmanCity={craftsmanItem.client.constructionCity}
-                craftsmanLogo={craftsmanItem.client.profilePicture}
-              />
-            ))
+          {craftsman.length > 0 ? (
+            craftsman
+              .filter((item) => item.craftsman !== null)
+              .map((item) => (
+                <CraftsmanContainer
+                  key={item._id}
+                  craftsmanName={item.craftsman.firstname}
+                  craftsmanAddress={item.craftsman.constructionAdress}
+                  craftsmanZip={item.craftsman.constructionZipCode}
+                  craftsmanCity={item.craftsman.constructionCity}
+                  craftsmanLogo={item.craftsman.profilePicture}
+                />
+              ))
           ) : (
-            <Text>Aucun client trouvé !</Text>
+            <Text>Aucun craftsman trouvé !</Text>
           )}
         </ScrollView>
       </View>
