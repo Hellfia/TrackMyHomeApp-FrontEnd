8b333a890e608182d3d02757b599dd3afa2d2e95
Hellfia <boulangerkevin@ymail.com>
2025-03-10 15:04:22 +0100

    mise en place du dashboard

--- screens/constructor/dashbaord/DashboardConstructeur.jsx
+++ screens/constructor/dashbaord/DashboardConstructeur.jsx
@@ -1,163 +1,163 @@
 import React, { useEffect, useState } from "react";
-import {
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-  
-} from "react-native";
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View, Image, Linking } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import Ionicons from "react-native-vector-icons/Ionicons";
-import globalStyles from "../../../styles/globalStyles";
+import { FontAwesome5 } from "@expo/vector-icons";
 import { useSelector } from "react-redux";
+import avatar from "../../../assets/avatar.png";
+import globalStyles from "../../../styles/globalStyles";
 
 export default function DashboardConstructeur({ navigation }) {
-  const [projectsData, setProjectsData] = useState([]);
+  const [projectsData, setProjectsData] = useState(0);
   const [clientsData, setClientsData] = useState([]);
+  const [craftsmenData, setCraftsmenData] = useState(0);
   const devUrl = process.env.DEV_URL;
   const constructeur = useSelector((state) => state.constructeur.value);
 
   useEffect(() => {
-    const constructorId = constructeur.constructorId;
-    fetch(`${devUrl}/projects/${constructorId}`)
+    fetch(`${devUrl}/constructors/${constructeur.token}`)
       .then((response) => response.json())
-      .then((data) => {
-        console.log('projects data', data)
-        setProjectsData(data.data.length);
-      })
-      .catch((error) => console.error(error));
+      .then((data) => setCraftsmenData(data.constructor.craftsmen.length))
+      .catch(console.error);
   }, []);
 
-
+  useEffect(() => {
+    fetch(`${devUrl}/projects/${constructeur.constructorId}`)
+      .then((response) => response.json())
+      .then((data) => setProjectsData(data.data.length))
+      .catch(console.error);
+  }, []);
 
   useEffect(() => {
-    const constructorId = constructeur.constructorId;
-  
-    fetch(`${devUrl}/projects/clients/${constructorId}`)
+    fetch(`${devUrl}/projects/clients/${constructeur.constructorId}`)
       .then((response) => response.json())
       .then((data) => {
-        console.log('clients data', data);
-  
         if (data.result) {
           setClientsData(
-            data.data.map((item) => ({
-              logo: item.client.profilePicture, // correction ici : 'profilePicture' au lieu de 'profilPicture'
+            data.data.map((item, index) => ({
+              id: index,
+              logo: item.client.profilePicture,
               firstname: item.client.firstname,
-              phoneNumber: item.client.phoneNumber || 'Non renseigné', // Ajouter un fallback si non existant
+              lastname: item.client.lastname,
+              phoneNumber: item.client.phoneNumber || "Non renseigné",
             }))
           );
         } else {
           setClientsData([]);
         }
       })
-      .catch((error) => {
-        console.error(error);
-        setClientsData([]);
-      });
+      .catch(console.error);
   }, []);
 
+  const callClient = (phoneNumber) => {
+    if (phoneNumber && phoneNumber !== "Non renseigné") {
+      Linking.openURL(`tel:${phoneNumber}`);
+    } else {
+      alert("Numéro de téléphone non renseigné");
+    }
+  };
 
   return (
     <SafeAreaView style={styles.container}>
-      <View>
-        <View style={globalStyles.header}>
-          <Text style={globalStyles.title}>Mon dashboard</Text>
+      <ScrollView contentContainerStyle={styles.content}>
+        <Text style={[globalStyles.title, { marginBottom: 20 }]}>Mon Dashboard</Text>
+
+        <Text style={styles.sectionTitle} onPress={() => navigation.navigate("Projet")}>Mes Chantiers</Text>
+        <View style={styles.section}>
+          <Text style={styles.sectionSubtitle}>
+            Nombre de chantier en cours :{' '}
+            </Text>
+          <Text style={styles.highlight}>{projectsData}</Text>
         </View>
-        <ScrollView style={styles.content}>
-          {/* // section pour le count du nombre de chantier */}
-          <View style={styles.section}>
-            <Text
-              style={styles.sectionTitle}
-              onPress={() => navigation.navigate("Projet")}
-            >
-              Mes Chantiers
-            </Text>
-            <Text style={styles.sectionSubtitle}>
-              Vous avez {projectsData} chantiers en cours
-            </Text>
-          </View>
-          {/* //section des clients */}
-          <View style={styles.section}>
-            <Text style={styles.sectionTitle}>Mes clients</Text>
-            <View style={styles.clientsRow}>
-              {clientsData.map((client) => (
-                <TouchableOpacity
-                  key={client.id}
-                  style={styles.clientCard}
-                  
-                >
-                  <Ionicons name="image-outline" size={40} color="#6C63FF" />
-                  <Text style={styles.clientText}>{client.firstname} { client.phoneNumber }</Text>
-                </TouchableOpacity>
-              ))}
-            </View>
-          </View>
-          {/* // affiche les messages */}
-          <View style={styles.section}>
-            <Text style={styles.sectionTitle}>Mes messages (0)</Text>
-            <Text style={styles.sectionSubtitle}>
-              Vous n'avez pas de nouveaux messages
-            </Text>
-          </View>
 
-          <View style={styles.section}>
-            <Text
-              style={styles.sectionTitle}
-              onPress={() => navigation.navigate("Documents")}
-            >
-              Mes documents
-            </Text>
+        <Text style={styles.sectionTitle}>Mes clients</Text>
+        <View style={styles.section}>
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            {clientsData.map((client) => (
+              <TouchableOpacity
+                key={client.id}
+                style={styles.clientCard}
+                onPress={() => callClient(client.phoneNumber)}
+              >
+                <Image
+                  source={client.logo ? { uri: client.logo } : avatar}
+                  style={styles.avatar}
+                />
+                <Text style={styles.clientText}>{client.firstname} {client.lastname}</Text>
+                <FontAwesome5 name="phone-alt" color="#FE5900" size={20} />
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
+        </View>
 
-            <Text style={styles.sectionSubtitle}>Mes documents</Text>
-            <View style={styles.clientsRow}></View>
-          </View>
-        </ScrollView>
-      </View>
+        <Text style={styles.sectionTitle}>Mes artisans</Text>
+        <TouchableOpacity style={styles.section} onPress={() => navigation.navigate("Intervenants")}>
+          <Text style={styles.sectionSubtitle}>Nombre d'artisan en activité:</Text>
+          <Text style={styles.highlight}>{craftsmenData}</Text>
+        </TouchableOpacity>
+      </ScrollView>
     </SafeAreaView>
   );
 }
 
-export const styles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "flex-start",
-    alignItems: "center",
+    backgroundColor: "#f5f5f5",
   },
   content: {
-    flex: 1,
-    padding: 20,
+    flexGrow: 1,
+    padding: 10,
   },
   section: {
-    marginBottom: 20,
-    padding: 12,
+    marginBottom: 30,
+    padding: 15,
     backgroundColor: "#fff",
-    borderRadius: 8,
+    borderRadius: 12,
+    borderWidth: 2,
+    borderColor: "#663ED9",
     shadowColor: "#000",
     shadowOpacity: 0.1,
-    shadowRadius: 4,
-    elevation: 2,
-  },
-  sectionTitle: {
-    fontSize: 16,
-    fontWeight: "600",
-    marginBottom: 6,
-    color: "#333",
-  },
-  sectionSubtitle: {
-    color: "#666",
-  },
-  clientsRow: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    marginTop: 10,
-  },
-  clientCard: {
-    width: 60,
+    shadowRadius: 6,
+    elevation: 3,
     alignItems: "center",
   },
-  clientText: {
-    marginTop: 4,
+  sectionTitle: {
+    fontSize: 20,
+    fontWeight: "600",
+    color: "#000",
+    marginBottom: 10,
+  },
+  sectionSubtitle: {
+    fontSize: 16,
     color: "#333",
+    textAlign: "center",
+  },
+  highlight: {
+    color: "#FE5900",
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  clientCard: {
+    borderWidth: 1,
+    borderColor: "#663ED9",
+    borderRadius: 8,
+    padding: 12,
+    marginRight: 12,
+    alignItems: "center",
+    width: 130,
+    backgroundColor: "#fff",
+  },
+  avatar: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    marginBottom: 6,
+  },
+  clientText: {
+    fontWeight: "500",
+    color: "#663ED9",
+    marginTop: 4,
+    marginBottom: 10,
+    textAlign: "center",
   },
 });
