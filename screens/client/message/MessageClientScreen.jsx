60abf163357e2a533e973edce0c0c816c51ac472
Benoit <gos.benoit@gmail.com>
2025-04-07 20:34:03 +0800

    screen message

---
+++ screens/client/message/MessageClientScreen.jsx
@@ -0,0 +1,6 @@
+import { StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import globalStyles from "../../../styles/globalStyles";
+export default function MessageClientScreen({}) {}
+return <SafeAreaView></SafeAreaView>;
+const styles = StyleSheet.create({});
---
+++ screens/constructor/message/MessageConstructeurScreen.jsx
@@ -0,0 +1,7 @@
+import { StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import globalStyles from "../../../styles/globalStyles";
+
+export default function MessageConstructeurScreen({}) {}
+return <SafeAreaView></SafeAreaView>;
+const styles = StyleSheet.create({});
